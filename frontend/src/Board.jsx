c9823a8cce000e62459341f3fda6f4f5bfcbbec5
Sachi Jitendra Hansalia <sachih@meta.com>
2025-06-18 11:57:59 -0700

    added router

--- frontend/src/Board.jsx
+++ frontend/src/Board.jsx
@@ -1,13 +1,15 @@
+import { Link } from 'react-router';
 import './Board.css'
 
 const Board = () => {
+    const id = 1;
     return(
         <>
         <div className="board">
             <img src="#" alt="board img"></img>
             <h3>Board Title</h3>
             <p>Board Category</p>
-            <button>View Board</button>
+            <button><Link to={`/board/${id}`}>View Board</Link></button>
             <button>Delete Board</button>
         </div>
         </>
--- frontend/src/BoardDetails.jsx
+++ frontend/src/BoardDetails.jsx
@@ -1,6 +1,10 @@
+import { Link } from 'react-router';
+import Card from './Card';
+
 const BoardDetails = () => {
     return (
         <>
+        <Link to='/'>Back to Board List</Link>
         <h2>Board Title</h2>
         <button>Create a Card</button>
         <Card/>
